b213e4057d3d3c92a0335c87088d108629a6f788
igor <igor.vila01@gmail.com>
2025-12-08 23:22:42 -0300

    colocando regras em algumas funçoes

--- sala.py
+++ sala.py
@@ -1,6 +1,13 @@
-alunos=[]
-def adicionar_aluno():
-    ...
+alunos={}
+def adicionar_aluno(nome, idade, curso):
+    alunos[nome]= {'idade': idade,
+                   'curso': curso,
+                   'notas': []
+                   }
+    
+def adicionar_nota(nome, nota):
+    if nome in alunos:
+        alunos[nome]['notas'].append(nota)
 
 def remover_aluno():
     ...
