0e09f83714c1deac18b577323bb3794f30c21498
Igor Vila <103008816+igorvila01@users.noreply.github.com>
2025-12-09 16:45:18 +0000

    pegar somente a primeira letra da entrada

--- sala.py
+++ sala.py
@@ -20,16 +20,17 @@ def listar_alunos():
 
 
 menu = """
-        [c] Cadastrar Aluno
-        [n] Adicionar Nota Aluno
-        [d] Remover Aluno
-        [m] Calcular Media Aluno 
-        [l] Listar Alunos
-        [s] Sair
-        =>"""
+[c] Cadastrar Aluno
+[n] Adicionar Nota Aluno
+[d] Remover Aluno
+[m] Calcular Media Aluno 
+[l] Listar Alunos
+[s] Sair
+=>"""
 
 while True:
-    menu_selecionado = input(menu)
-    
+    entrada = input(menu)[0].lower()
+    print(entrada)
+
 
     break
